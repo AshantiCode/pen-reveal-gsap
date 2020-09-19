8860e0da81bbe555b64f6d3aecb70d2532ce0e14
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-19 21:18:59 +0200

    add scrolltrigger to upper pen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-function init(){
-    
-    // start here
-
+function init() {
+  gsap.set(".part3", {
+    y: () => {
+      return -(document.querySelector(".pen-top").clientHeight - 22);
+    },
+    scrollTrigger: {
+      id: "pen-body",
+      trigger: ".part3",
+      start: "top bottom-=270",
+      end: `+=${document.querySelector(".pen-top").clientHeight - 22}`,
+      pin: true,
+      markers: true,
+    },
+  });
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+  init();
 });
