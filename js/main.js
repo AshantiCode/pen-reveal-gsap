fd5263be6a369bf796786313272c0a57593fa894
Ashanti Haring <ashanti.haring@gmail.com>
2020-09-20 13:14:56 +0200

    add animation to tip of pen, start with parts within

--- js/main.js
+++ js/main.js
@@ -14,10 +14,47 @@ function init() {
       pinSpacing: false,
     },
   });
-
+  // push the part into the body as starting points
   gsap.set(".part4", { y: -548 });
   gsap.set(".part5", { y: -722 });
   gsap.set(".part6", { y: -843 });
+
+  // Tween the tip of the pen back to 0
+  gsap.to(".part6", {
+    y: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".pen-body",
+      start: "top bottom-=640",
+      end: "+=843", // same as we offsetted it line 20
+      scrub: true, // while we scroll it animates
+      markers: true,
+    },
+  });
+
+  gsap.to(".part5", {
+    y: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".pen-body",
+      start: "top bottom-=640",
+      end: "+=722 ", // same as we offsetted it line 20
+      scrub: true, // while we scroll it animates
+      markers: true,
+    },
+  });
+
+  gsap.to(".part4", {
+    y: 0,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".pen-body",
+      start: "top bottom-=640",
+      end: "+=548", // same as we offsetted it line 20
+      scrub: true, // while we scroll it animates
+      markers: true,
+    },
+  });
 } // end init
 
 window.addEventListener("load", function () {
